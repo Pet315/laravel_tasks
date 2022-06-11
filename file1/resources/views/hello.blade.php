3c76e1ccdf3b3be5a4f58d780004f5ee060bf014
Petro Shpylovyi <79025915+Pet315@users.noreply.github.com>
2022-06-12 00:52:35 +0300

    Update hello.blade.php

--- file1/resources/views/hello.blade.php
+++ file1/resources/views/hello.blade.php
@@ -1,11 +1,11 @@
 <html>
     <body>
         <h8>
-            <br><br><br>
             -------------------
             <br>Hello, dear user! Welcome to the game "Unexpected Trap"! This game is about
             secret actions that you should do in levels to win. If you need some help or explanation,
             please write us on gmail: unextr_game@gmail.com."
         </h8>
+        @yield('base')
     </body>
 </html>
